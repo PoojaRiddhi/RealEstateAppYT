c7468ebb2f511ffd9a3b25b40fa269c8b92b5302
PoojaRiddhi <kankatepooja05@gmail.com>
2025-02-26 18:55:40 +0000

    # project main fjles

---
+++ projectDirFile/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MauiFirstApp.View;
+using MauiFirstApp.ViewModel;
+
+namespace MauiFirstApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(root: new LandingPage());
+        }
+    }
+}
